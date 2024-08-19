e904cd6133d8045bd9741520bd95724df1a76093
chris <weavc@pm.me>
2024-08-19 13:50:49 +0100

    Experimenting with function pointers & bitmasks

--- rotate_2d_array.c
+++ rotate_2d_array.c
@@ -2,7 +2,12 @@
 #include <stdlib.h>
 #include <math.h>
 
-static int size = 25;
+#define DIRECTION_X 1
+#define DIRECTION_Y 2
+
+
+// static variables are scoped to this file, even if defined inside a function
+static int size = 5;
 
 typedef struct {
     short ** arr;
@@ -10,13 +15,16 @@ typedef struct {
 } arr_2d;
 
 static arr_2d * new_set(int s) {
+    // dynamic assignment of short[]
     short ** arr = (short **) malloc(s * sizeof(short *));
+
     for (int i = 0; i < s; i++) {
         arr[i] = (short *) malloc(s * sizeof(short));
         for (int j = 0; j < s; j++) {
             arr[i][j] = '0';
         }
     }
+
     arr_2d * r = malloc(sizeof(arr_2d *));
     r->arr = arr;
     r->size = s;
@@ -36,7 +44,8 @@ static void dispose_set(arr_2d *set) {
     free(set);
 }
 
-static void render_set(arr_2d *set) {
+// const is used in this case to show the value cannot be changed
+static void render_set(const arr_2d *set) {
     printf("===========\n");
     for (int i = 0; i < set->size; i++) {
         printf("%d:\t", i);
@@ -48,60 +57,78 @@ static void render_set(arr_2d *set) {
     printf("===========\n");
 }
 
-static short * get_between(arr_2d *set, int c1[2], int c2[2], int size) {
-    short * curr = (short *) malloc(size * sizeof(short));
-    int x = 1;
-    int from = c1[0];
-    int to = c2[0];
-    if (from == to) {
-        x = 0;
-        from = c1[1];
-        to = c2[1];
+typedef struct {
+    int step;
+    int counter;
+    int from;
+    int to;
+} walk_counter;
+
+typedef void (*walk_func)(const arr_2d *set, short direction, int index, walk_counter * x_counter, walk_counter * y_counter, const void * args);
+
+static void walk_between(const arr_2d *set, int corner1[2], int corner2[2], int size, walk_func func, const void * args) {
+    short direction = 0;
+    walk_counter x_counter;
+    x_counter.step = 1;
+    x_counter.counter = corner1[0];
+    x_counter.from = corner1[0];
+    x_counter.to = corner2[0];
+    walk_counter y_counter;
+    y_counter.step = 1;
+    y_counter.counter = corner1[1];
+    y_counter.from = corner1[1];
+    y_counter.to = corner2[1];
+
+    if (corner1[0] != corner2[0]) {
+        direction = direction | DIRECTION_X;
     }
 
+    if (corner2[0] < corner1[0]) {
+        x_counter.step = -1;
+    }
+
+    if (corner1[1] != corner2[1]) {
+        direction = direction | DIRECTION_Y;
+    }
+
+    if (corner2[1] < corner1[1]) {
+        y_counter.step = -1;
+    }
+
+    walk_counter * xc = &x_counter;
+    walk_counter * yc = &y_counter;
+    
     for (int p = 0; p < size; p++) {
-        if (x == 0) {
-            curr[p] = set->arr[from][c1[0]];
-        } else {
-            curr[p] = set->arr[c1[1]][from];
-        }
-
-        if (to < from) {
-            from--;
-        } else {
-            from++;
-        }
-    }
-
-    return curr;
-}
-
-static void set_between(arr_2d *set, int c1[2], int c2[2], int size, short * vals) {
-    int x = 1;
-    int from = c1[0];
-    int to = c2[0];
-    if (from == to) {
-        x = 0;
-        from = c1[1];
-        to = c2[1];
-    }
-
-    for (int p = 0; p < size; p++) {
-        if (x == 0) {
-            set->arr[from][c1[0]] = vals[p];
-        } else {
-            set->arr[c1[1]][from] = vals[p];
-        }
-
-        if (to < from) {
-            from--;
-        } else {
-            from++;
-        }
+        yc->counter = yc->from + (yc->step*p);
+        xc->counter = xc->from + (xc->step*p);
+        func(set, direction, p, xc, yc, args);
     }
 }
 
-static void rot_array(arr_2d *set, int size, int depth) {
+static void set_walk_func(const arr_2d *set, short direction, int index, walk_counter * x_counter, walk_counter * y_counter, const void * args) {
+    const short * vals = (short *) args;
+
+    if (direction & DIRECTION_Y) {
+        set->arr[y_counter->counter][x_counter->to] = vals[index];
+    } 
+    
+    if (direction & DIRECTION_X) {
+        set->arr[y_counter->to][x_counter->counter] = vals[index];
+    }
+}
+
+static void get_walk_func(const arr_2d *set, short direction, int index, walk_counter * x_counter, walk_counter * y_counter, const void * args) {
+    short * curr = (short *) args;
+    if (direction & DIRECTION_Y) {
+        curr[index] = set->arr[y_counter->counter][x_counter->to];
+    } 
+    
+    if (direction & DIRECTION_X) {
+        curr[index] = set->arr[y_counter->to][x_counter->counter];
+    }
+}
+
+static void rot_array(const arr_2d *set, int size, int depth) {
     int m = size-1;
     int zero = depth;
 
@@ -110,20 +137,20 @@ static void rot_array(arr_2d *set, int size, int depth) {
     short ** rset = (short **) malloc(4 * sizeof(short *));
 
     for (int i = 0; i < 4; i++) {
-        rset[i] = get_between(set, c[i], c[(i+1)%4], size);
+        rset[i] = (short *) malloc(size * sizeof(short));
+        walk_between(set, c[i], c[(i+1)%4], size, get_walk_func, rset[i]);
     }
 
     for (int i = 0; i < 4; i++) {
-        set_between(set, c[(i+1)%4], c[(i+2)%4], size, rset[i]);
+        walk_between(set, c[(i+1)%4], c[(i+2)%4], size, set_walk_func, rset[i]);
     }
 
     dispose_2d_arr(rset, 4);
 
     if (m-1 > 1) {
         rot_array(set, m-1, depth+1);
-    }
+    } 
 }
-
 
 static inline int get_start(int max) {
     int p = 1;
@@ -133,7 +160,7 @@ static inline int get_start(int max) {
     return pow(10, p);
 }
 
-static void set_seq_array(arr_2d *set) {
+static void set_seq_array(const arr_2d *set) {
     int start = get_start(pow(set->size, 2));
 
     for (int i = 0; i < set->size; i++) {
@@ -143,7 +170,7 @@ static void set_seq_array(arr_2d *set) {
     }
 }
 
-static void set_number_array(arr_2d *set, short val) {
+static void set_number_array(const arr_2d *set, short val) {
     int start = get_start(pow(set->size, 2));
 
     for (int i = 0; i < set->size; i++) {
@@ -153,7 +180,7 @@ static void set_number_array(arr_2d *set, short val) {
     }
 }
 
-void rot_set(arr_2d *set) {
+void rot_set(const arr_2d *set) {
     rot_array(set, set->size, 0);
 }
 
